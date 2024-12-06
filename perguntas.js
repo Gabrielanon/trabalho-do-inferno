def6192cab21f99792179de4a2d4f907d35059f9
Gabrielanon <141028810+Gabrielanon@users.noreply.github.com>
2024-12-06 20:50:18 -0300

    ok

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que é java?',
+    'linguagem de programação orientada a objeto'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da Inglaterra?',
+    'A capital da Inglaterra é Londres'
+)
+
+criaCartao(
+    'Armamento',
+    'Qual o calibre do fal?',
+    'calibre 7.62'
+)
+
+criaCartao(
+    'Musíca',
+    'Qual a melhor banda de todas?',
+    'Pink Floyd'
+)
